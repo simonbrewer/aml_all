9f7bacd6242a5b037beaaa56e38d483f3ffe5641
Simon Brewer <brewersi@gmail.com>
2022-07-24 14:48:19 -0600

    Building tuning spaces

--- 02_tuning_all.R
+++ 02_tuning_all.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(mlr3verse)
 library(mlr3tuning)
 library(mlr3tuningspaces)
+library(mlr3pipelines)
 library(paradox)
 
 ## ----------------------------------------------------------------------------
@@ -32,6 +33,27 @@ task_all <- TaskClassif$new(id = "all", backend = dat2,
 as.data.table(mlr_tuning_spaces)
 
 ## -------------------------------------------------------------------------- ##
+## RF tuning (without OHE)
+# tune learner with default search space
+instance_rf = tune(
+  method = "grid_search",
+  task = task_all,
+  learner = lts(lrn("classif.ranger")),
+  resampling = rsmp ("cv", folds = 5),
+  measure = msr("classif.bacc"),
+  term_evals = 100
+)
+
+# best performing hyperparameter configuration
+instance_rf$result
+
+## -------------------------------------------------------------------------- ##
+## Pipeline with OHE
+encode <- po("encode")
+
+g1 = GraphLearner$new(encode %>>% po(lrn("classif.glmnet")))
+
+## -------------------------------------------------------------------------- ##
 ## glmnet tuning
 # tune learner with default search space
 instance_glmnet = tune(
@@ -47,18 +69,20 @@ instance_glmnet = tune(
 instance_glmnet$result
 
 ## -------------------------------------------------------------------------- ##
-## RF tuning
+## xgb tuning
 # tune learner with default search space
-instance_rf = tune(
-  method = "grid_search",
+instance_xgb = tune(
+  method = "random_search",
   task = task_all,
-  learner = lts(lrn("classif.ranger")),
+  learner = lts(lrn("classif.xgboost")),
   resampling = rsmp ("cv", folds = 5),
   measure = msr("classif.bacc"),
-  term_evals = 100
+  term_evals = 200
 )
 
 # best performing hyperparameter configuration
-instance_rf$result
+instance_xgb$result
 
+# save(instance_glmnet, instance_rf, instance_xgb, 
+#      file = "tuned_models.RData")
 
